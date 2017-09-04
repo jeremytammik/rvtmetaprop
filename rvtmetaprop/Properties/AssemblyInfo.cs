d6d42558f9ce2b4d6e3db58a16d348a29cd7eaac
Jeremy Tammik <jeremy.tammik@autodesk.com>
2017-09-04 12:32:51 +0200

    added history and description

--- rvtmetaprop/Properties/AssemblyInfo.cs
+++ rvtmetaprop/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle( "rvtmetaprop" )]
-[assembly: AssemblyDescription( "Revit Add-In Description for rvtmetaprop" )]
+[assembly: AssemblyDescription( "Revit C# .NET add-in to import and store meta properties created in Forge" )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "Autodesk Inc." )]
 [assembly: AssemblyProduct( "rvtmetaprop Revit C# .NET Add-In" )]
@@ -32,5 +32,22 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "2018.0.0.0" )]
-[assembly: AssemblyFileVersion( "2018.0.0.0" )]
+//
+// History:
+//
+// 2018.0.0.0 still working on skeleton
+// 2018.0.0.1 bim element access, csv and json deserialisation works
+// 2018.0.0.2 added int and double property types, parameter definition data and simple logging class
+// 2018.0.0.3 implemented my own message logging
+// 2018.0.0.4 implemented transaction group and separate transaction to set existing properties
+// 2018.0.0.5 implemented FileSelector and CreateSharedParameters
+// 2018.0.0.6 implemented setting of properties
+// 2018.0.0.7 successful first test completed
+// 2018.0.0.8 added check for read-only parameter
+// 2018.0.0.9 cleaned up logging
+// 2018.0.0.10 ignore file and link properties
+// 2018.0.0.11 skip shared param creation for delete request
+// 2018.0.0.12 added urban house sample model and first test on http://meta-editor.autodesk.link/
+//
+[assembly: AssemblyVersion( "2018.0.0.12" )]
+[assembly: AssemblyFileVersion( "2018.0.0.12" )]
